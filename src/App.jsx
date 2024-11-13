a817a0dc2d57e55e4b5960743ba8e855cc93d06b
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-11-13 06:25:37 +0300

    adding form and inputs to my artwork form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-
 import NavBar from "./components/NavBar"
-
+import AddWorkPage from "./components/ArtWorkPage";
 
 function App() {
 
   return (
+    <div>
    <NavBar/>
-  
+   <AddWorkPage/>
+   </div>
   );
 }
 
--- src/components/AddArtWorkForm.jsx
+++ src/components/AddArtWorkForm.jsx
@@ -1,6 +1,17 @@
 function AddArtWorkForm(){
     return(
-        <div></div>
+        <div className="Add-Artwork-form">
+            <h2>New ArtWork</h2>
+            <form>
+                <input type="text" name="name" placeholder="Title of the art" /> 
+                <input type="text" name="name" placeholder="Artist" />
+                <input type="text" name="image" placeholder="" />
+                <input type="text" name="price" placeholder="Price" />
+                <button type="submit">Post Art</button>
+
+            </form>
+
+        </div>
     )
 }
 
